16b2b5d569e4680c7fbcaa045a7e7a06af521c68
zhenerenya <zhenyachmchk@gmail.com>
2021-01-06 21:13:40 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/multi_kriterial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/multi_kriterial.dir/main.cpp.o"
+  "multi_kriterial"
+  "multi_kriterial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/multi_kriterial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
